11ee41670d0fd9e23acd28b2eec62ee2e4608358
Arjun Thimmareddy <athimma@ncsu.edu>
2016-03-02 22:18:16 -0500

    Support for the chrome plugin

--- simpl-shot-server/src/main/java/com/simplshot/server/FileServer.java
+++ simpl-shot-server/src/main/java/com/simplshot/server/FileServer.java
@@ -1,11 +1,14 @@
 package com.simplshot.server;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
 
+import javax.imageio.ImageIO;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -13,11 +16,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.codec.binary.Base64InputStream;
 import org.apache.commons.io.IOUtils;
+import org.apache.xmlgraphics.util.io.Base64DecodeStream;
 import org.bson.types.ObjectId;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
+import com.lowagie.text.pdf.codec.Base64;
 import com.simplshot.mongo.MongoUtil;
 import com.simplshot.ocr.OcrUtility;
 
@@ -50,7 +56,10 @@ public class FileServer {
 		File userDirectory = new File(strBuffer.toString());
 		if(!userDirectory.exists())
 			userDirectory.mkdirs();
-		strBuffer.append(contentDispositionHeader.getFileName());
+		if(contentDispositionHeader.getFileName() == null)
+			strBuffer.append(""+System.currentTimeMillis());
+		else
+			strBuffer.append(contentDispositionHeader.getFileName());
 		if(directory.exists() && saveFile(fileInputStream,strBuffer.toString()))
 		{
 			String extracts = performOcrProcessing(strBuffer.toString());/*Need to remove stopwords*/
@@ -60,7 +69,10 @@ public class FileServer {
 			ObjectId mongoId = new ObjectId();
 			String awsFileUrl;
 			try {
-				awsFileUrl = awsHandler.uploadFile(strBuffer.toString(), userName+mongoId+contentDispositionHeader.getFileName());
+				if(contentDispositionHeader.getFileName() == null)
+					awsFileUrl = awsHandler.uploadFile(strBuffer.toString(), userName+mongoId+System.currentTimeMillis());	
+				else
+					awsFileUrl = awsHandler.uploadFile(strBuffer.toString(), userName+mongoId+contentDispositionHeader.getFileName());
 			} catch (IOException ex) {
 				LOGGER.severe("Error Saving to aws");
 				ex.printStackTrace();
@@ -68,6 +80,39 @@ public class FileServer {
 			}
 			/*Upload and get link*/
 			MongoUtil.getInstance().addLinkToUser(mongoId,userName,awsFileUrl,extracts);/*url*/
+			return Response.status(200).entity(SUCCESS).build();
+		}else
+		{
+			return Response.status(500).entity(ERROR).build();
+		}
+	}
+	
+	@POST
+	@Path("/chrome/file")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response uploadFileFromChrome(@FormDataParam("attachment") String fileInputStream,@FormDataParam("attachment") FormDataContentDisposition contentDispositionHeader, @FormDataParam("USER") String userName)
+	{
+		File directory = new File(AppStart.UPLOAD_DIR);
+		strBuffer.append(userName+"\\");
+		File userDirectory = new File(strBuffer.toString());
+		if(!userDirectory.exists())
+			userDirectory.mkdirs();
+		strBuffer.append(""+System.currentTimeMillis());
+		if(directory.exists() && saveBase64File(fileInputStream,strBuffer.toString()))
+		{
+			AwsHandler awsHandler = new AwsHandler();
+			ObjectId mongoId = new ObjectId();
+			String awsFileUrl;
+			try {
+				awsFileUrl = awsHandler.uploadFile(strBuffer.toString()+".jpg", userName+mongoId);	
+			} catch (IOException ex) {
+				LOGGER.severe("Error Saving to aws");
+				ex.printStackTrace();
+				return Response.status(500).entity(ERROR).build();
+			}
+			/*Upload and get link*/
+			MongoUtil.getInstance().addLinkToUser(mongoId,userName,awsFileUrl,"");/*url*/
 			return Response.status(200).entity(SUCCESS).build();
 		}else
 		{
@@ -86,6 +131,26 @@ public class FileServer {
 				fileOut.flush();
 				fileOut.close();
 				IOUtils.closeQuietly(inpStream);
+				return true;
+			} catch (IOException e) {
+				LOGGER.severe("Error Saving File to disk");
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	private boolean saveBase64File(String inpStream, String outputFile)
+	{
+		LOGGER.info(inpStream);
+		if(inpStream != null && !outputFile.equals(AppStart.UPLOAD_DIR))
+		{
+			try {
+				byte[] imgBytes = Base64.decode(inpStream); 
+				FileOutputStream fileOut = new FileOutputStream(outputFile+".jpg");
+				fileOut.write(imgBytes);
+				fileOut.flush();
+				fileOut.close();
 				return true;
 			} catch (IOException e) {
 				LOGGER.severe("Error Saving File to disk");
