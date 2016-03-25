eae9c59f405b86e79abc27cdda9efce27a51b269
Arjun Thimmareddy <athimma@ncsu.edu>
2016-03-25 16:45:11 -0400

    Adding new files for supporting telemetry and cross searching

---
+++ simpl-shot-server/src/main/java/com/simplshot/mongo/ResponseStatus.java
@@ -0,0 +1,21 @@
+package com.simplshot.mongo;
+
+public class ResponseStatus {
+	
+	private String success;
+	private String reason;
+	
+	public String getSuccess() {
+		return success;
+	}
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+}
